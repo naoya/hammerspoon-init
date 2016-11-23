f4edb4242935b8b73b813a6b21753729a4bcda64
Naoya Ito <i.naoya@gmail.com>
2016-11-24 05:59:45 +0900

    First commit

---
+++ init.lua
@@ -0,0 +1,61 @@
+require('watcher')
+
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      -- hs.alert.show(name)
+      if name ~= "iTerm2" then
+         enableAllHotkeys()
+      else
+         disableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+-- カーソル移動
+remapKey({'ctrl'}, 'f', keyCode('right'))
+remapKey({'ctrl'}, 'b', keyCode('left'))
+remapKey({'ctrl'}, 'n', keyCode('down'))
+remapKey({'ctrl'}, 'p', keyCode('up'))
+
+-- テキスト編集
+remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
+remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))
+
+-- コマンド
+remapKey({'ctrl'}, 's', keyCode('f', {'cmd'}))
+remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))
+remapKey({'ctrl'}, 'g', keyCode('escape'))
+
+-- ページスクロール
+remapKey({'ctrl'}, 'v', keyCode('pagedown'))
+remapKey({'alt'}, 'v', keyCode('pageup'))
+remapKey({'cmd', 'shift'}, ',', keyCode('home'))
+remapKey({'cmd', 'shift'}, '.', keyCode('end'))
